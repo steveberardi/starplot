387428851d02c865bc20153cba691e92f4241eef
Steve Berardi <sberardi@gmail.com>
2024-04-04 10:38:02 -0700

    tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,3 +1,5 @@
+import pytest
+
 from starplot.models import Star
 
 
@@ -28,3 +30,24 @@ def test_star_false_expressions():
         (Star.name == "wrong") | (Star.name != "fakestar"),
     ]
     assert not any([e.evaluate(star) for e in expressions])
+
+def test_model_manager_get():
+    sirius = Star.get(name="Sirius")
+
+    assert sirius.magnitude == -1.44
+    assert sirius.hip == 32349
+
+def test_model_manager_get_raises_exception():
+    with pytest.raises(ValueError):
+        Star.get(name=None)
+
+def test_model_manager_find():
+    hipstars = Star.find(where=[Star.hip.is_not_null()])
+    assert len(hipstars) == 118_218
+
+    names = {"Sirius", "Bellatrix", "Castor", "Vega"}
+    bright = Star.find(where=[Star.name.is_in(names)])
+    assert len(bright) == 4
+    assert set([s.name for s in bright]) == names
+
+
