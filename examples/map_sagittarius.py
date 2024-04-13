92b019bbabe9838c5ba46391716e6fff688ed5a7
Steve Berardi <6608085+steveberardi@users.noreply.github.com>
2024-04-13 10:06:07 -0700

    improve

--- examples/map_sagittarius.py
+++ examples/map_sagittarius.py
@@ -9,16 +9,21 @@ style = PlotStyle().extend(
             "font_name": "GFS Didot",
             "font_size": 7,
         },
+        "dso_open_cluster": {
+            "marker": {
+                "size": 17,
+            },
+        },
     },
 )
 p = MapPlot(
     projection=Projection.MILLER,
     ra_min=15.6,
     ra_max=19.8,
-    dec_min=-54,
-    dec_max=-8,
+    dec_min=-51.6,
+    dec_max=-3,
     style=style,
-    resolution=2800,
+    resolution=3000,
 )
 p.constellations()
 
@@ -26,22 +31,32 @@ p.stars(
     where=[Star.magnitude <= 3],
     style__marker__size=72,
     style__marker__symbol="star_8",
+    style__marker__zorder=200,
 )
 p.stars(
     where=[
         Star.magnitude > 3,
-        Star.magnitude < 12,
+        Star.magnitude < 11,
     ],
     style__marker__size=12,
     bayer_labels=True,
     catalog="tycho-1",
 )
 
-p.dsos(where=[DSO.magnitude.is_null() | (DSO.magnitude < 12)], true_size=True)
-
+p.nebula(
+    where=[
+        DSO.magnitude.is_null() | (DSO.magnitude < 12),
+    ],
+    true_size=True,
+)
+p.open_clusters(
+    where=[
+        DSO.magnitude.is_null() | (DSO.magnitude < 12),
+    ],
+    true_size=False,
+)
 p.constellation_borders()
 p.ecliptic()
 p.celestial_equator()
 p.milky_way()
-p.gridlines(tick_marks=True)
-p.export("map_sagittarius.png", padding=0.2)
+p.export("map_sagittarius.png", padding=0.08)
