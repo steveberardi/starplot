fba518afdd669259aa6fc03ba49e70b885c222a6
Steve Berardi <6608085+steveberardi@users.noreply.github.com>
2024-04-22 08:09:05 -0700

    tutorial styling

--- tutorial/tutorial_05.py
+++ tutorial/tutorial_05.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from pytz import timezone
 from starplot import OpticPlot
-from starplot.optics import Refractor
+from starplot.optics import Binoculars
 from starplot.styles import PlotStyle, extensions
 
 dt = datetime.now(timezone("US/Pacific")).replace(2024, 4, 8, 21, 0, 0)
@@ -9,24 +9,25 @@ dt = datetime.now(timezone("US/Pacific")).replace(2024, 4, 8, 21, 0, 0)
 style = PlotStyle().extend(
     extensions.GRAYSCALE_DARK,
     extensions.OPTIC,
+    {"star": {"marker": {"size": 58}}}, # make the stars bigger
 )
 
 p = OpticPlot(
-    # M44
+    # target location - M44
     ra=8.667,
     dec=19.67,
+    # observer location - Palomar Mountain
     lat=33.363484,
     lon=-116.836394,
-    # Refractor Telescope
-    optic=Refractor(
-        focal_length=560,
-        eyepiece_focal_length=14,
-        eyepiece_fov=82,
+    # define the optic - 10x binoculars with a 65 degree field of view
+    optic=Binoculars(
+        magnification=10,
+        fov=65,
     ),
     dt=dt,
     style=style,
     resolution=1600,
 )
-p.stars(mag=14, catalog="tycho-1", bayer_labels=True)
-# p.info()
+p.stars(mag=12, catalog="tycho-1", bayer_labels=True)
+
 p.export("tutorial_05.png", padding=0.1, transparent=True)
