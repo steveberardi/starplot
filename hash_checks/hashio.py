16ff855a09b60ec6a5c606c078ddd986093107b1
Steve Berardi <sberardi@gmail.com>
2024-08-31 19:22:50 -0700

    spawn

--- hash_checks/hashio.py
+++ hash_checks/hashio.py
@@ -1,6 +1,7 @@
 import sys
 import inspect
 import time
+import multiprocessing as mp
 
 from multiprocessing import Pool
 from pathlib import Path
@@ -87,9 +88,10 @@ class Hashio:
 
     def _get_hashes(self) -> dict:
         """Gets hashes for all callables"""
+        mp.set_start_method('spawn') # required for M1 macs? or macOS issue?
 
         console.print("Getting hashes...", style="bold")
-        with Pool(5) as p:
+        with mp.Pool(5) as p:
             results = p.map(self._call_wrapper, self.callables)
 
         return {func_name: hashes for func_name, hashes in results}
