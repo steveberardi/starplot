0a8fbc9d8f93dafe0271f366419ca34469df81f1
Steve Berardi <6608085+steveberardi@users.noreply.github.com>
2024-09-07 13:35:12 -0700

    edge color

--- src/starplot/styles/base.py
+++ src/starplot/styles/base.py
@@ -551,6 +551,7 @@ class PlotStyle(BaseStyle):
     figure_background_color: ColorStr = ColorStr("#fff")
 
     text_border_width: int = 3
+    text_border_color: ColorStr = ColorStr("#fff")
     text_offset_x: float = 0.005
     text_offset_y: float = 0.005
 
