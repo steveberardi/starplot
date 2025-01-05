0f51f47880a0a4f62fb66bc8e9052ea73825a816
Steve Berardi <6608085+steveberardi@users.noreply.github.com>
2025-01-05 13:11:56 -0800

    fix schema on read

--- src/starplot/data/bigsky.py
+++ src/starplot/data/bigsky.py
@@ -14,10 +14,9 @@ DIGITS = 4
 
 
 def download(
-    download_path: str = None,
+    download_path: str = str(DOWNLOAD_PATH / BIG_SKY_FILENAME),
     digits: int = 4,
 ):
-    download_path = download_path or str(DOWNLOAD_PATH / BIG_SKY_FILENAME)
     utils.download(
         BIG_SKY_URL,
         download_path,
@@ -30,7 +29,7 @@ def download(
     )
 
 
-def to_parquet(source_path: str, destination_path: str, digits: int = DIGITS):
+def to_parquet(source_path: str, destination_path: str):
     import pyarrow as pa
 
     print("Preparing Big Sky Catalog for Starplot...")
@@ -38,7 +37,7 @@ def to_parquet(source_path: str, destination_path: str, digits: int = DIGITS):
     df = pd.read_csv(
         source_path,
         header=0,
-        names=[
+        usecols=[
             "tyc_id",
             "hip_id",
             "ccdm",
@@ -53,22 +52,31 @@ def to_parquet(source_path: str, destination_path: str, digits: int = DIGITS):
         compression="gzip",
     )
 
-    schema = pa.schema(
-        [
-            ("tyc_id", pa.string()),
-            ("hip_id", pa.int32()),
-            ("ccdm", pa.string()),
-            ("magnitude", pa.float16()),
-            ("bv", pa.float16()),
-            ("ra_degrees_j2000", pa.float16()),
-            ("dec_degrees_j2000", pa.float16()),
-            ("ra_mas_per_year", pa.float16()),
-            ("dec_mas_per_year", pa.float16()),
-            ("parallax_mas", pa.float16()),
-        ]
+    # df = df.astype(dtype= {
+    #         'tyc_id': "object", 
+    #         'hip_id': "int64",
+    #         "ccdm": "object",
+    #         "magnitude": "float64",
+    #         "bv": "float64",
+    #         "ra_degrees_j2000": "float64",
+    #         "dec_degrees_j2000": "float64",
+    #         "ra_mas_per_year": "float64",
+    #         "dec_mas_per_year": "float64",
+    #         "parallax_mas": "float64",
+    #     })
+   
+
+    df = df.assign(epoch_year=2000)
+
+    df = df.rename(
+        columns={
+            "hip_id": "hip",
+            "ra_degrees_j2000": "ra_degrees",
+            "dec_degrees_j2000": "dec_degrees",
+        }
     )
 
-    df.to_parquet(destination_path, engine="pyarrow", schema=schema, compression="gzip")
+    df.to_parquet(destination_path, engine="pyarrow", compression="gzip")
 
     print(f"Done! {destination_path}")
 
