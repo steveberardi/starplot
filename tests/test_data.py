78e43a511a3379314d9a058ed26839a6ed397703
Steve Berardi <6608085+steveberardi@users.noreply.github.com>
2025-01-11 12:08:21 -0800

    fix

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,14 +1,15 @@
 import importlib
+import os
+
+from unittest import mock
+from starplot import data, settings
 
 
+@mock.patch.dict(os.environ, {"STARPLOT_DOWNLOAD_PATH": "/testing"})
+def test_download_path():
+    importlib.reload(settings) # must reload this first
+    importlib.reload(data)
 
-
-def test_download_path(monkeypatch):
-    monkeypatch.setenv("STARPLOT_DOWNLOAD_PATH", "/testing")
-
-    # importlib.reload(data)
-    # importlib.reload(settings)
-    from starplot import data, settings
-
+    assert os.environ.get("STARPLOT_DOWNLOAD_PATH") == "/testing"
     assert str(settings.DOWNLOAD_PATH) == "/testing"
     assert str(data.DataFiles.BIG_SKY).startswith("/testing")
