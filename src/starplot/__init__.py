5f18e5d5faba30025af803cb9ad988f5c0de303c
Steve Berardi <6608085+steveberardi@users.noreply.github.com>
2025-01-18 09:49:34 -0800

    imports

--- src/starplot/__init__.py
+++ src/starplot/__init__.py
@@ -8,6 +8,7 @@ from .horizon import HorizonPlot  # noqa: F401
 from .optic import OpticPlot  # noqa: F401
 from .models import (
     DSO,  # noqa: F401
+    DsoType,  # noqa: F401
     Star,  # noqa: F401
     Constellation,  # noqa: F401
     Planet,  # noqa: F401
--- src/starplot/models/__init__.py
+++ src/starplot/models/__init__.py
@@ -1,5 +1,5 @@
 from .constellation import Constellation  # noqa: F401,F403
-from .dso import DSO  # noqa: F401,F403
+from .dso import DSO, DsoType  # noqa: F401,F403
 from .star import Star  # noqa: F401,F403
 from .planet import Planet  # noqa: F401,F403
 from .moon import Moon  # noqa: F401,F403
