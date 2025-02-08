26571fdc9a4ceffa06f261827e8bd6b0b58865e3
Steve Berardi <6608085+steveberardi@users.noreply.github.com>
2025-02-08 07:01:09 -0800

    more strict versions

--- src/starplot/__init__.py
+++ src/starplot/__init__.py
@@ -1,6 +1,6 @@
 """Star charts and maps of the sky"""
 
-__version__ = "0.15.2"
+__version__ = "0.15.3"
 
 from .base import BasePlot  # noqa: F401
 from .map import MapPlot, Projection  # noqa: F401
