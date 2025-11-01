d248f18cefe0321f0eb498f2628b1a5a5b0ea59f
Steve Berardi <sberardi@gmail.com>
2025-11-01 07:39:21 -0700

    test settings

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,4 +1,4 @@
-from starplot import settings, ZenithPlot
+from starplot import ZenithPlot, settings, override_settings
 
 
 def test_settings_language_valid():
@@ -6,3 +6,16 @@ def test_settings_language_valid():
 
     p = ZenithPlot()
     assert p.language == "fr"
+
+    settings.language = "en-us"
+
+def test_override_settings():
+    assert settings.svg_text_type == "path"
+    assert settings.language == "en-us"
+
+    with override_settings(svg_text_type="element", language="zh-cn"):
+        assert settings.svg_text_type == "element"
+        assert settings.language == "zh-cn"
+    
+    assert settings.svg_text_type == "path"
+    assert settings.language == "en-us"
