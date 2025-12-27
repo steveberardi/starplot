c414214b9b62fd505fe4139d231399faf614bb72
Steve Berardi <sberardi@gmail.com>
2025-12-27 06:11:02 -0800

    clean up

--- src/starplot/data/db.py
+++ src/starplot/data/db.py
@@ -11,12 +11,13 @@ NAME_TABLES = {
 }
 
 
-# @cache
 def connect():
     connection = duckdb.connect(
         DataFiles.DATABASE,
         read_only=True,
-    )  # , threads=2, memory_limit="1GB"
+        # threads=4,
+        # memory_limit="1GB",
+    )
 
     path = settings.data_path / "duckdb-extensions"
     connection.raw_sql(f"SET extension_directory = '{str(path)}';")
